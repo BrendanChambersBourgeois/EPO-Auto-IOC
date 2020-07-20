ab043ce16066b3e4f878ba1ccfa8aceecf19bff4
BrendanChambersBourgeois <39083193+BrendanChambersBourgeois@users.noreply.github.com>
2020-07-21 02:49:25 +1000

    Small update
    More var's more implicit for fun.
    Removed some new lines

--- EPO.Auto.cs
+++ EPO.Auto.cs
@@ -7,24 +7,24 @@ namespace EPO_Auto_IOC
 {
     internal class Program
     {
-        static void Main()
+        private static void Main()
         {
             // Build stringbuilder for encoded strings
             StringBuilder encodedStrings = BuildString();
             // Load a MimeMessage from string builder
-            var message = MimeMessage.Load(GenerateStreamFromString(DecodeBase64(encodedStrings.ToString())));
+            MimeMessage message = MimeMessage.Load(GenerateStreamFromString(DecodeBase64(encodedStrings.ToString())));
             foreach (MimeEntity attachment in message.Attachments)
             {
                 if (attachment is MessagePart)
                 {
-                    var part = (MessagePart)attachment;
+                    MessagePart part = (MessagePart)attachment;
                     Console.WriteLine(part);
                 }
                 else
                 {
-                    var part = (MimePart)attachment;
-                    var filename = part.Content.Open();
-                    using var reader = new StreamReader(filename);
+                    MimePart part = (MimePart)attachment;
+                    Stream filename = part.Content.Open();
+                    using StreamReader reader = new StreamReader(filename);
                     string value = reader.ReadToEnd();
                     Console.WriteLine(value);
                 }
@@ -33,7 +33,7 @@ namespace EPO_Auto_IOC
 
         private static StringBuilder BuildString()
         {
-            var encodedStrings = new System.Text.StringBuilder();
+            StringBuilder encodedStrings = new StringBuilder();
             while (true)
             {
                 // Read standed in
@@ -46,22 +46,22 @@ namespace EPO_Auto_IOC
                 // add each string to stringbuilder
                 else
                 {
-                    encodedStrings.AppendLine(encodedString.ToString());
+                    _ = encodedStrings.AppendLine(encodedString.ToString());
                 }
             }
-
             return encodedStrings;
         }
 
         public static Stream GenerateStreamFromString(string s)
         {
-            var stream = new MemoryStream();
-            var writer = new StreamWriter(stream);
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream);
             writer.Write(s);
             writer.Flush();
             stream.Position = 0;
             return stream;
         }
+
         private static string DecodeBase64(string base64EncodedValue)
         {
             byte[] data = Convert.FromBase64String(base64EncodedValue);
