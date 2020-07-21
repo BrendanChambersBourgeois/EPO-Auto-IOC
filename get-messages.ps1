0f626c532b763e95315eaa4d2ad30f19b41b32ca
BrendanChambersBourgeois <39083193+BrendanChambersBourgeois@users.noreply.github.com>
2020-07-21 17:31:25 +1000

    IOC array
    Added IOC array

--- get-messages.ps1
+++ get-messages.ps1
@@ -1,25 +1,35 @@
 ﻿#connect-ExchangeOnline -ShowProgress $true
 #Install-Package MimeKit
 
-function Get-Messages{ 
+function Get-Messages
+{ 
       Param (
           [Parameter(Mandatory=$false)]
           # Date 'MM/dd/yyyy'
           [String]$StartReceivedDate = (Get-Date).addMonths(-1)
       )
-$m = {}
-$callm = {}
-#QuarantineMessage
-$m = Get-QuarantineMessage -StartReceivedDate (Get-Date).AddDays(-30) -PageSize 100 -QuarantineTypes malware
-#QuarantineMessage with html/htm - likly call phishing
-$callm = $m | Preview-QuarantineMessage | where {$_.attachment -like '*.html' -or $_.attachment -like '*.htm'}
-foreach($call in $callm){
-$call_identity = $call | select -ExpandProperty Identity
-$call_file_path = $call_identity -replace "\\", "-"
-$pwd = Get-Location
-$tmp = Export-QuarantineMessage -Identity $call_identity
-$attachment = $tmp.eml | .".\EPO-Auto-IOC\EPO-Auto-IOC.exe"
-$attachment | Out-File -FilePath .\attachments\$call_file_path
-write-host $call_file_path 'saved'
-}
+    $m = {}
+    $callm = {}
+    #QuarantineMessage
+    $m = Get-QuarantineMessage -StartReceivedDate (Get-Date).AddDays(-30) -PageSize 100 -QuarantineTypes malware
+    #QuarantineMessage with html/htm - likly call phishing
+    $callm = $m | Preview-QuarantineMessage | where {$_.attachment -like '*.html' -or $_.attachment -like '*.htm'}
+    $Array = @()
+    $urls = @()
+    foreach($call in $callm)
+    {
+        $call_identity = $call | select -ExpandProperty Identity
+        $call_file_path = $call_identity -replace "\\", "-"
+        $tmp = Export-QuarantineMessage -Identity $call_identity
+
+        $Result = "" | Select Identity,ReceivedTime,SenderAddress,Subject,Attachment,urls
+        $Result.Identity = $call.Identity
+        $Result.ReceivedTime = $call.ReceivedTime
+        $Result.SenderAddress = $call.SenderAddress
+        $Result.Subject = $call.Subject
+        $Result.Attachment = $call.Attachment
+        $Result.urls += $tmp.eml | .".\EPO-Auto-IOC\EPO-Auto-IOC.exe"
+        $Array += $Result
+    }
+$Array
 }
